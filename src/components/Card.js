b607bf834d75a802f56430523173dafd7ce44e71
Shubham <shubhamshivaay67@gmail.com>
2022-01-21 20:12:22 +0530

    alt text added

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Card() {
     return (
         <div className="card">
-            <img src="../images/katy-zaferes.png" className="card--image" />
+            <img src="../images/katy-zaferes.png" className="card--image" alt="katy zaferes"/>
             <div className="card--stats">
-                <img src="../images/star.png" />
+                <img src="../images/star.png" alt="star"/>
                 <span>5.0</span>
                 <span>(6) • </span>
                 <span>USA</span>
